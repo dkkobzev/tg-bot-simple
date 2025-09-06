96b098115092a3ca96e53ad5522f434aa72a874a
dkkobzev <1132231936@rudn.ru>
2025-09-06 11:45:18 +0300

    second commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import telebot
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    raise RuntimeError("В .env нет TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я твой первый бот! Напиши /help")
+
+@bot.message_handler(commands=['help'])
+def help_cmd(message):
+    bot.reply_to(message, "/start - начать\n/help - помощь")
+
+if __name__ == "__main__":
+    bot.infinity_polling(skip_pending=True)
